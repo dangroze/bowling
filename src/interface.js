6ab0a6f0472369979d013b8aabe689290621ff1d
Dan Groze <dangroze@gmail.com>
2019-03-19 22:08:21 +0000

    Add function for number of pins in table

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   var game = new Game();
+  
+  $('#submit-btn').click(function() {
+    let frameValue = $('#frame').val();
+    let rollValue = $('#roll').val();
+    let pinsValue = $('#pins').val();
+    $('#' + frameValue + '-' + rollValue +  '-' + 'pins').text(pinsValue);
+  });
 });

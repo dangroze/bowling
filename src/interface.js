7d6386a2b19909a0d2f4fdb2363cf641bf67aeb6
Dan Groze <dangroze@gmail.com>
2019-03-19 23:18:57 +0000

    Table shows score with no spares or strikes

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
   var game = new Game();
-  
+
   $('#submit-btn').click(function() {
     let frameValue = $('#frame').val();
     let rollValue = $('#roll').val();
     let pinsValue = $('#pins').val();
     $('#' + frameValue + '-' + rollValue +  '-' + 'pins').text(pinsValue);
+    updateFrame(frameValue, rollValue, pinsValue);
+    updateTotalScore();
   });
+
+  function updateFrame(frameValue, rollValue, pinsValue) {
+    let pinsInteger = parseInt(pinsValue)
+    if (rollValue === '1') {
+      var currentFrame = new Frame(pinsInteger);
+      game.addFrame(currentFrame);
+    } else {
+      game.frames[frameValue - 1].addSecondRoll(pinsInteger);
+    };
+   };
+
+   function updateTotalScore() {
+      let totalScore = game.gameScore();
+      $('#total-score').text(totalScore);
+   };
 });
